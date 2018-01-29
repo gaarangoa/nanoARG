3971bd8cd4aeb9847c8da8fc9f8bfd0fcc58f654
gaarangoa <gaarangoa@gmail.com>
2018-01-29 18:29:09 -0500

    fix

--- backend/scheduler/remote/conf.py
+++ backend/scheduler/remote/conf.py
@@ -1,4 +1,4 @@
 tools = "/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/tools/"
 data = "/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/data/"
-add_python_path = '/groups/metastorm_cscee/nanoARG/backend/scheduler/remote'
+add_python_path = '/groups/metastorm_cscee/nanoARG/backend/scheduler/'
 main_pipeline = "/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/pipeline.py"
