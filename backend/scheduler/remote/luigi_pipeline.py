0cb4d52f7f65ae78f36f4d9392aa868706abbc64
gaarangoa <gaarangoa@gmail.com>
2018-01-30 14:06:05 -0500

    fix

--- backend/scheduler/remote/luigi_pipeline.py
+++ backend/scheduler/remote/luigi_pipeline.py
@@ -6,7 +6,7 @@ import json
 class CreateEnv(luigi.Task):
     stage = 'CREATING ENVIRONMENT IN CLUSTER'
     parameters = luigi.Parameter()
-    parameters = json.loads(base64.b64decode(parameters)
+    parameters = json.loads(base64.b64decode(parameters))
 
     def output(self):
         return luigi.LocalTarget(parameters['remote_input_file'])
