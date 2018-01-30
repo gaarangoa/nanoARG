e2cb836a2681b78c7e17193847698fe417d52939
gaarangoa <gaarangoa@gmail.com>
2018-01-30 15:59:03 -0500

    fix

--- backend/scheduler/remote/luigi_pipeline.py
+++ backend/scheduler/remote/luigi_pipeline.py
@@ -11,12 +11,13 @@ class CreateEnv(luigi.Task):
     
     def output(self):
         par = json.loads(base64.b64decode(self.parameters))
-        return luigi.LocalTarget(par['remote_input_file'])
+        return luigi.LocalTarget(par['remote_input_file']+".log")
 
     def run(self):
         par = json.loads(base64.b64decode(self.parameters))
         os.system( "mkdir -p "+par['storage_remote_dir'] )
         os.system( "ssh newriver1.arc.vt.edu python "+par['remote_path']+"/retrieve_file.py "+self.parameters )
+        os.system( ' echo "done" >> 'par['remote_input_file']+".log")
 
 
 
