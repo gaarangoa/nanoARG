7b2083f3a2c6f81e3022654c715cde7b4a15026c
gaarangoa <gaarangoa@gmail.com>
2018-01-31 14:21:38 -0500

    fix

--- backend/scheduler/remote/tools/deepARGClass.py
+++ backend/scheduler/remote/tools/deepARGClass.py
@@ -42,7 +42,7 @@ class DeepARG():
             if "#" in i[0]: continue
             i = i.split()
             j = i[3].split("---")
-            item = "\t".join([j[1], i[5], i[7], j[2], j[3], j[4], j[5], j[6], j[7], j[8] ])+"\n"
+            item = "\t".join( [ j[1], i[5], i[7], j[2], j[3], j[4], j[5], j[6], j[7], j[8], i[9], i[10] ])+"\n"
             fo.write( item )
         os.system("mv " + input_file+".dl.tmp " + input_file)
             
