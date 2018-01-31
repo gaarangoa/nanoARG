98acaf40f0228c47a3ca99ab66b30d408ed4a9fd
gaarangoa <gaarangoa@gmail.com>
2018-01-31 09:44:37 -0500

    fix

--- backend/scheduler/remote/pipelines/argClass.py
+++ backend/scheduler/remote/pipelines/argClass.py
@@ -13,16 +13,16 @@ class ARGs():
         self.database_name = "deeparg"
         self.aligner = ALIGNER();
         self.reference = conf.data+self.database_name+".dmnd"
-        self.alignment_file = input+"."+self.database_name+".mge.aln"
-        self.observable_file = input+"."+self.database_name+".mge.aln.bed.clusters.bestHit.annotated."+self.database_name+".json"
-        self.postprocess_file = input+"."+self.database_name+".mge.alg.annotated."+self.database_name+".json"
+        self.alignment_file = input+"."+self.database_name+".aln"
+        self.observable_file = input+"."+self.database_name+".aln.bed.clusters.bestHit.annotated."+self.database_name+".json"
+        self.postprocess_file = input+"."+self.database_name+".alg.annotated."+self.database_name+".json"
     
     def align(self):
         parameters= {
             "--iden":30,
             "--prob":0.8,
             "--evalue": 1e-10,
-            "--coverage": 0.5,
+            "--coverage": 0.0,
             "--nk": 200
         }
         self.aligner.align(self.input, self.reference, self.alignment_file, parameters)
