4bf8eebc5f1e2d162e15e1fe2e24063e8ba46a64
gaarangoa <gaarangoa@gmail.com>
2018-01-31 18:35:04 -0500

    fix

--- backend/scheduler/remote/validation/GetGeneSize.py
+++ backend/scheduler/remote/validation/GetGeneSize.py
@@ -7,5 +7,5 @@ fi=sys.argv[1] # fastafile from besthit
 sep = sys.argv[2] # separator to get only sequence id
 
 for record in SeqIO.parse(open(fi), "fasta"):
-    id = record.id.split(':')[0]
+    id = record.id.split(sep)[0]
     print ">"+id+"\t"+str(len(record.seq))
