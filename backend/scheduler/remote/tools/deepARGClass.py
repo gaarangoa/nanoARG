4a2bbbb09461eb7146802b5ceaff3ff0daf0be11
gaarangoa <gaarangoa@gmail.com>
2018-01-30 20:54:03 -0500

    fix

--- backend/scheduler/remote/tools/deepARGClass.py
+++ backend/scheduler/remote/tools/deepARGClass.py
@@ -9,7 +9,7 @@ class DeepARG():
 
     def align(self, query, reference, output, parameters):
         options = " ".join([i+" "+str(parameters[i]) for i in parameters])
-        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && "+ self.bin + "python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --align --genes --type nucl --input "+query+" --output "+output+"  "+ options
+        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && "+" python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --align --genes --type nucl --input "+query+" --output "+output+"  "+ options
         print("running deepARG:", cmd)
         os.system(cmd)
 
