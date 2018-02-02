ddc7023c39979c5bc68346c4d096419882cc290c
gaarangoa <gaarangoa@gmail.com>
2018-02-02 13:27:11 -0500

    added coverage to the report

--- backend/scheduler/remote/validation/blast2bed.py
+++ backend/scheduler/remote/validation/blast2bed.py
@@ -4,12 +4,14 @@ def blast2bed(input, arglen, iden, evalue, cov, bitscore):
     ARGLen = {i.split()[0].split("|")[0].replace('>',""):int(i.split()[1]) for i in open(arglen)} # File with the genes sizes, for computing the coverage.
     fo = open(input+'.bed','w');
     fo2 = open(input+'.sel', 'w');
+    # fo.write("#read\tqstart\tqend\treference\tbitscore\tstrand\tAlignment-length__Evalue__Identity__Coverage__Reference-gene-length\n")
     for i in open(input):
         i = i.split();
         rule = 0
+        coverage = int(i[3])/float(ARGLen[i[1].split('|')[0]])
         if float(i[2]) < iden: continue # minimum identity 60
         if float(i[10]) > evalue: continue # minimum evalue 1e-10
-        if int(i[3])/float(ARGLen[i[1].split('|')[0]]) < cov: continue # minimum coverage 90% ARG
+        if coverage < cov: continue # minimum coverage 90% ARG
         if float(i[-1]) < bitscore: continue # minimum bitscore to consider
         # print int(i[3]), ARGLen['>'+i[1]], cov*ARGLen['>'+i[1]] 
         # Initial location and strand 
@@ -23,7 +25,9 @@ def blast2bed(input, arglen, iden, evalue, cov, bitscore):
             end=i[7];
         
         # TODO: make sure that the last term of the list does not affect other process!
-        extra = "_".join([i[3], i[-2], i[2]])
+        extra = "_".join([i[3], i[-2], i[2], str(coverage), str(ARGLen[i[1].split('|')[0]])])
+        
+        # read qstart qend reference bitscore strand Alignment-length__Evalue__Identity__Coverage__Reference-gene-length
         wt = "\t".join([i[0], start, end, i[1], i[-1], strand, extra])+'\n'
         fo.write(wt)
         
