9ed5910bcbea1bc41c5e43d3eb89c9102459f03d
gaarangoa <gaarangoa@gmail.com>
2018-02-09 14:40:14 -0500

    ffix

--- backend/scheduler/remote/pipelines/TaxonomyClass.py
+++ backend/scheduler/remote/pipelines/TaxonomyClass.py
@@ -1,8 +1,6 @@
 
 from tools.centrifugeClass import Centrifuge as ALIGNER
-from tools.bestLocalHitClass import BestLocalHit as BestHit
 import conf
-
 
 class Taxonomy():
     def __init__(self, input, database):
@@ -13,19 +11,38 @@ class Taxonomy():
         self.reference = conf.data+self.database_name
         self.alignment_file = input+"."+self.database_name+".aln"
         self.observable_file = input+"."+self.database_name+".aln.bed.clusters.bestHit.annotated."+self.database_name+".json"
-        self.postprocess_file = input+"."+self.database_name+".alg.annotated."+self.database_name+".json"
+        # self.postprocess_file = input+"."+self.database_name+".alg.annotated."+self.database_name+".json"
     
     def align(self):
         parameters= {
             "-f":""
         }
-        
+
         self.aligner.align(self.input, self.reference, self.alignment_file, parameters)
 
     def postprocess(self):
-        # annotation = BestHit(conf.data+self.database_name+".size", _IDEN, _EVALUE, _COVERAGE, _BITSCORE)
-        # annotation.quant(self.alignment_file, self.database_name)
-        pass
+        
+        taxo_data = {}
+        for i in open(self.alignment_file):
+            i = i.split()
+            taxo_data[i[1]] = {
+                "name": i[0],
+                "tax_id": i[1],
+                "rax_rank": i[2],
+                "genome_size": int(i[3]),
+                "num_reads": int(i[4]),
+            }
+
+        reads_data = {}
+        for i in open(self.alignment_file+'.reads'):
+            i = i.split()
+            reads_data[i[0]] = {
+                "id": i[0],
+                "tax_id": i[2],
+                "qlength": int(i[6]),
+            }
+        
+        json.dump(open({"taxo": taxo_data, "reads": reads_data}, self.observable_file))
 
 
 
