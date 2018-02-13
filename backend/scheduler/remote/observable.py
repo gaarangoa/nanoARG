0c8bf0021e45b8118ed993a186a909fef93b1361
gaarangoa <gaarangoa@gmail.com>
2018-02-13 12:22:42 -0500

    update observable cluster

--- backend/scheduler/remote/observable.py
+++ backend/scheduler/remote/observable.py
@@ -5,7 +5,8 @@ import requests
 
 parameters = json.loads(base64.b64decode(sys.argv[1]))
 
-
-
+cmd = 'http://bench.cs.vt.edu/api/nanoarg/sample/status/'+parameters['projectID']+'/'+parameters['_id']+'/done'
+r = requests.get(cmd)
+print(r.json())
 
 
