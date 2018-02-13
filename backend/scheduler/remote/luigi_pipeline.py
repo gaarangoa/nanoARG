6f4469502b26ba680278556b516a8990ff889ee5
gaarangoa <gaarangoa@gmail.com>
2018-02-13 14:19:50 -0500

    fix

--- backend/scheduler/remote/luigi_pipeline.py
+++ backend/scheduler/remote/luigi_pipeline.py
@@ -117,5 +117,5 @@ class RetrieveResults(luigi.Task):
 def mourn_failure(task, exception):
     parameters = task.parameters;
     par = json.loads(base64.b64decode(parameters))
-    print("----------------- ERROR ------------8-8--8-8-8-8--8-8-8-8-8-8-8-8-8-8-8-8-8-8-8-8--88-")
-    # os.system( "ssh newriver1.arc.vt.edu python "+par['remote_path']+"/observable.py "+self.parameters + " " + task.stage+":FAIL" )
+    # print("----------------- ERROR ------------8-8--8-8-8-8--8-8-8-8-8-8-8-8-8-8-8-8-8-8-8-8--88-")
+    os.system( "ssh newriver1.arc.vt.edu python "+par['remote_path']+"/observable.py "+self.parameters + " " + task.stage+":FAIL" )
