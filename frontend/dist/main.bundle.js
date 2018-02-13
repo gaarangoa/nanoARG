8eb3c5f5d5c572b11748ca453a88a741b6e5b7d3
gaarangoa <gaarangoa@gmail.com>
2018-02-12 19:30:08 -0500

    update

--- frontend/dist/main.bundle.js
+++ frontend/dist/main.bundle.js
@@ -2277,13 +2277,9 @@ var ViewSamplesComponent = (function () {
 
 "use strict";
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return environment; });
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `angular-cli.json`.
 var environment = {
-    production: false,
-    api_url: 'http://localhost:27856',
+    production: true,
+    api_url: 'http://bench.cs.vt.edu/api/nanoarg/',
     upload_dir: ''
 };
 //# sourceMappingURL=/Volumes/drive/projects/ARG/nanopore/frontend/src/environment.js.map
