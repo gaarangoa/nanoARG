a673bab6205f92a5e944803aae7ede325e1a9581
gaarangoa <gaarangoa@gmail.com>
2018-02-13 12:39:37 -0500

    fix

--- backend/scheduler/remote/observable.py
+++ backend/scheduler/remote/observable.py
@@ -5,7 +5,7 @@ import requests
 
 parameters = json.loads(base64.b64decode(sys.argv[1]))
 
-cmd = 'http://bench.cs.vt.edu/api/nanoarg/sample/status/'+parameters['projectID']+'/'+parameters['_id']+'/done'
+cmd = 'http://bench.cs.vt.edu/api/nanoarg/sample/status/'+parameters['_id']+'/'+parameters['projectID']+'/done'
 r = requests.get(cmd)
 print(r.json())
 
