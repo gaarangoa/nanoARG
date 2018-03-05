20b33dcf05c9e723b107e4a8197208ec1383a156
gaarangoa <gaarangoa@gmail.com>
2018-03-05 12:00:49 -0500

    fix

--- frontend/src/app/project/view-samples/view-samples.component.ts
+++ frontend/src/app/project/view-samples/view-samples.component.ts
@@ -284,7 +284,7 @@ export class ViewSamplesComponent implements OnInit {
                 item.data['sample'] = sample['name'];
                 item.data['rel_abn_args'] = (item['data'].size * 1 / res[4]['total_mapped_ARG_reads']).toFixed(5);
                 item.data['rel_abn_total_genes'] = (item['data'].size * 1 / res[4]['total_functional_reads']).toFixed(5); 
-                item.data['rel_abn_total_reads'] = (item['data'].size * 1 / res[4]['total_reads']).toFixed(10);
+                item.data['rel_abn_total_reads'] = (item['data'].size * 10000 / res[4]['total_reads']).toFixed(5);
                 item.data['rel_abn_unique_strains'] = (item['data'].size * 1 / res[4]['total_unique_genomes']).toFixed(5);
                 item.data['category'] = item['data']['metadata'][3];
                 this.all_samples.push(item.data);
