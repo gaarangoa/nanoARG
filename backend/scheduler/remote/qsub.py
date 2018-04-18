b7af1328fd80ab1ea747602e253913bd1a654685
gaarangoa <gaarangoa@gmail.com>
2018-04-18 13:29:57 -0400

    no need to request limit on memory cluster

--- backend/scheduler/remote/qsub.py
+++ backend/scheduler/remote/qsub.py
@@ -12,26 +12,29 @@ os.system('mkdir -p '+parameters['storage_remote_dir'])
 qsub = parameters['qsub_file']
 
 cmd = "\n".join([
-        '#!/bin/bash',
-        '#PBS -l nodes=1:ppn=6,pmem=20gb',
-        '#PBS -l walltime=20:00:00',
-        # '#PBS -q open_q',
-        '#PBS -q normal_q',
-        '#PBS -A computeomics',
-        '#PBS -W group_list=newriver\n',
-        'module load gcc/5.2.0  openmpi/1.8.5 hmmer bedtools\n',
-        'cd '+ parameters['storage_remote_dir'],
-        'export PYTHONPATH=$PYTHONPATH:'+parameters['remote_path'],
-        'export CENTRIFUGE_HOME='+parameters['remote_path']+'/bin/centrifuge-1.0.3-beta',
-        'source /groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/activate',
-        "/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/luigid --background --logdir=logs",
-        'cd '+ parameters['remote_path'],
-        '/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/luigi --module '+ parameters['pipeline'] + " RetrieveResults " + " --local-scheduler --parameters " + sys.argv[1],
-        'exit;'
-        ]);
+    '#!/bin/bash',
+    '#PBS -l nodes=1:ppn=6',
+    '#PBS -l walltime=20:00:00',
+    # '#PBS -q open_q',
+    '#PBS -q normal_q',
+    '#PBS -A computeomics',
+    '#PBS -W group_list=newriver\n',
+    'module load gcc/5.2.0  openmpi/1.8.5 hmmer bedtools\n',
+    'cd ' + parameters['storage_remote_dir'],
+    'export PYTHONPATH=$PYTHONPATH:'+parameters['remote_path'],
+    'export CENTRIFUGE_HOME=' + \
+    parameters['remote_path']+'/bin/centrifuge-1.0.3-beta',
+    'source /groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/activate',
+    "/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/luigid --background --logdir=logs",
+    'cd ' + parameters['remote_path'],
+    '/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/environment/bin/luigi --module ' + \
+    parameters['pipeline'] + " RetrieveResults " + \
+    " --local-scheduler --parameters " + sys.argv[1],
+    'exit;'
+])
 
-outq = open(qsub,'w');
-outq.write(cmd);
-outq.close();
+outq = open(qsub, 'w')
+outq.write(cmd)
+outq.close()
 
 os.system('cd '+parameters['storage_remote_dir']+' && qsub '+qsub)
