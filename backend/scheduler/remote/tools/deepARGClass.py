50198efa1a9a26f54673973308d487d15f820436
gaarangoa <gaarangoa@gmail.com>
2018-08-16 20:14:34 -0400

    added deepARG probability

--- backend/scheduler/remote/tools/deepARGClass.py
+++ backend/scheduler/remote/tools/deepARGClass.py
@@ -1,6 +1,9 @@
-import sys, os
+import sys
+import os
 import conf
 from validation.blast2bed import blast2bed
+
+
 class DeepARG():
     def __init__(self):
         self.bin = conf.tools
@@ -10,22 +13,26 @@ class DeepARG():
 
     def align(self, query, reference, output, parameters):
         options = " ".join([i+" "+str(parameters[i]) for i in parameters])
-        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && "+" python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --align --genes --type nucl --input "+query+" --output "+output+"  "+ options
+        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && " + \
+            " python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --align --genes --type nucl --input " + \
+            query+" --output "+output+"  " + options
         print("running deepARG:", cmd)
         try:
             os.system(cmd)
         except Exception as inst:
-            os.system('echo "'+str(inst)+" >> "+ output)
+            os.system('echo "'+str(inst)+" >> " + output)
         return True
 
     def predict(self, query, reference, output, parameters):
         options = " ".join([i+" "+str(parameters[i]) for i in parameters])
-        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && "+" python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --predict --genes --type nucl --input "+query+".tmp --output "+output+"  "+ options
+        cmd = "source /groups/metastorm_cscee/deeparg/venv/bin/activate && " + \
+            " python /groups/metastorm_cscee/deeparg/deeparg-ss/deepARG.py  --predict --genes --type nucl --input " + \
+            query+".tmp --output "+output+"  " + options
         print("running deepARG:", cmd)
         try:
             os.system(cmd)
         except Exception as inst:
-            os.system('echo "'+str(inst)+" >> "+ output)
+            os.system('echo "'+str(inst)+" >> " + output)
         return True
 
     def preprocess(self, input_file, arglen_file):
@@ -37,7 +44,7 @@ class DeepARG():
         cmd = 'sort -k1,1 -k2,2n '+fi+'|'+'bedtools cluster -s -d 0 >'+fi+'.pre.clusters'
         os.system(cmd)
 
-        for ix,i in enumerate(open(fi+".pre.clusters")):
+        for ix, i in enumerate(open(fi+".pre.clusters")):
             i = i.split()
             j = i[-2].split("_")
             start = i[1]
@@ -45,7 +52,8 @@ class DeepARG():
             if i[5] == "_":
                 start = i[2]
                 end = i[1]
-            item = [i[0]+"_cluster_"+i[-1], i[3], j[2], j[0], j[7], j[8], start, end, j[5], j[6], j[1], i[4]]
+            item = [i[0]+"_cluster_"+i[-1], i[3], j[2], j[0],
+                    j[7], j[8], start, end, j[5], j[6], j[1], i[4]]
             fo.write("\t".join(item)+"\n")
 
         fo.close()
@@ -54,19 +62,20 @@ class DeepARG():
 
     def postprocess(self, input_file):
         print(input_file)
-        map_aln = {(i.split()[0], i.split()[1]):i.split() for i in open(input_file+'.tmp')}
+        map_aln = {(i.split()[0], i.split()[1]): i.split()
+                   for i in open(input_file+'.tmp')}
         # convert map file to blast output file
         fo = open(input_file+".dl.tmp", "w")
         for i in open(input_file+".dl.ARG"):
-            if "#" in i[0]: continue
+            if "#" in i[0]:
+                continue
             i = i.split()
             try:
-                j = map_aln[ (i[3], i[5]) ]
-                item = "\t".join( [ i[3].split("_cluster_")[0], i[5] ]+j[2:])+"\n"
-                fo.write( item )
+                j = map_aln[(i[3], i[5])]
+                item = "\t".join([i[3].split("_cluster_")[0],
+                                  i[5]] + j[2:] + [i[6]])+"\n"
+                fo.write(item)
             except Exception as e:
-                print('deepARG: this read does not have a proper best hit '+"\t".join(i) )
+                print('deepARG: this read does not have a proper best hit '+"\t".join(i))
 
         # os.system("mv " + input_file+".dl.tmp " + input_file)
-
-
