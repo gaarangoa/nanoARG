7da91687e1d1b3094715d3aea274e2cebc1a5c7c
gaarangoa <gaarangoa@gmail.com>
2018-09-26 12:04:16 -0400

    added log to consumer

--- backend/scheduler/consumer/consumer.py
+++ backend/scheduler/consumer/consumer.py
@@ -9,6 +9,22 @@ __local_dir__ = "/src/"
 import subprocess
 import base64
 
+import logging
+
+log_file = logging.FileHandler(
+    filename='/src/consumer.log'
+)
+log_stdout = logging.StreamHandler(sys.stdout)
+handlers = [log_file, log_stdout]
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(levelname)s %(asctime)s - %(message)s",
+    handlers=handlers
+)
+
+log = logging.getLogger()
+
 
 class CONSUMER:
     def __init__(self, consumer_id):
@@ -52,6 +68,8 @@ class CONSUMER:
                          "/"+body['projectID']+"/"+body["_id"]+"/qsub.sh"})
             body.update({'pipeline': local.config.pipeline})
 
+            log.info(('body of job', body))
+
             item = base64.b64encode(json.dumps(body))
 
             # # # check whether there is space in the cluster to run more jobs
@@ -62,14 +80,18 @@ class CONSUMER:
             cmd = "ssh "+local.config.ssh_host+" mkdir -p " + \
                 body['storage_remote_dir']
             os.system(cmd)
+            log.info('crate environment')
+            log.info(cmd)
             # send the input file
             cmd = "scp "+body['local_input_file']+" " + \
                 body['remotehost']+":"+body['remote_input_file']
             os.system(cmd)
+            log.info('send input file')
             # run the pipeline
             cmd = "ssh "+local.config.ssh_host+" python " + \
                 local.config.remote_tools + "/qsub.py " + item
             os.system(cmd)
+            log.info('run the pipeline')
 
             ch.basic_ack(delivery_tag=method.delivery_tag)
 
@@ -89,7 +111,7 @@ while True:
         consumer = CONSUMER("consumer")
         consumer.run()
     except Exception as inst:
-        print(inst)
+        log.error(str(inst))
 
 # method_frame, header_frame, body = channel.basic_get(queue="hello")
 # if method_frame:
