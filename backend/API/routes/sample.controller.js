bd43274da634044da2a49355041796d5e619e57b
Gustavo Arango <gustavo1@Gustavos-MacBook-Pro.local>
2019-06-26 13:20:37 -0400

    update download full dataset

--- backend/API/routes/sample.controller.js
+++ backend/API/routes/sample.controller.js
@@ -11,30 +11,35 @@ var exec = require('child_process').exec;
 
 var remote_host = "gustavo1@newriver1.arc.vt.edu:/groups/metastorm_cscee/nanoARG/backend/scheduler/remote/storage/";
 
-router.get('/:sampleID', function(req, res, next) {
+router.get('/:sampleID', function (req, res, next)
+{
     var sampleID = req.params['sampleID'];
     // console.log(sampleID)
     sample.readElementByID(sampleID)
         .then(
-            function(response) {
+            function (response)
+            {
                 res.json(response);
             }
         );
 });
 
-router.get('/read/results/:sample_id', function(req, res, next) {
+router.get('/read/results/:sample_id', function (req, res, next)
+{
     var sample_id = req.params['sample_id'];
 
     sample.readElementByID(sample_id)
         .then(
-            function(response) {
+            function (response)
+            {
                 // console.log(response)
 
                 var fi = '/src/data/' + response[0].projectID + '_' + sample_id + '.json';
                 var fi2 = '/src/data/' + response[0].projectID + '_' + sample_id + '.min.json';
 
                 cmd = 'ls -lstg ' + fi;
-                var dir = exec(cmd, function(err, stdout, stderr) {
+                var dir = exec(cmd, function (err, stdout, stderr)
+                {
                     if (err) {
                         res.json({
                             message: 'still working',
@@ -49,7 +54,8 @@ router.get('/read/results/:sample_id', function(req, res, next) {
                         // If the results file is too big to be processed
                         if (fileSizeInMegabytes >= 20) {
 
-                            fs.readFile(fi2, 'utf8', function(err, data) {
+                            fs.readFile(fi2, 'utf8', function (err, data)
+                            {
                                 if (err) {
                                     res.json({
                                         status: false,
@@ -68,7 +74,8 @@ router.get('/read/results/:sample_id', function(req, res, next) {
                             });
 
                         } else {
-                            fs.readFile(fi, 'utf8', function(err, data) {
+                            fs.readFile(fi, 'utf8', function (err, data)
+                            {
                                 if (err) {
                                     res.json({
                                         status: false,
@@ -103,11 +110,13 @@ router.get('/read/results/:sample_id', function(req, res, next) {
 });
 
 
-router.get('/read/results/download/:sample_id', function(req, res, next) {
+router.get('/read/results/download/:sample_id', function (req, res, next)
+{
     var sample_id = req.params.sample_id;
     sample.readElementByID(sample_id)
         .then(
-            function(response) {
+            function (response)
+            {
                 // console.log(response)
                 var fi = '/src/data/' + response[0].projectID + '_' + sample_id + '.json';
                 res.download(fi);
@@ -115,19 +124,22 @@ router.get('/read/results/download/:sample_id', function(req, res, next) {
         );
 });
 
-router.get('/project/:projectID', function(req, res, next) {
+router.get('/project/:projectID', function (req, res, next)
+{
     var sampleID = req.params['projectID'];
     // console.log(sampleID)
     sample.readSamplesByProjectID(sampleID)
         .then(
-            function(response) {
+            function (response)
+            {
                 res.json(response);
             }
         );
 });
 
 
-router.post('/update/status', function(req, res, next) {
+router.post('/update/status', function (req, res, next)
+{
     var sampleID = req.body._id;
     var status = req.body.status;
     sample.updateElementByID(sampleID, {
@@ -141,7 +153,8 @@ router.post('/update/status', function(req, res, next) {
 });
 
 
-router.post('/update/results', function(req, res, next) {
+router.post('/update/results', function (req, res, next)
+{
     var sampleID = req.body._id;
     var results = req.body.results;
     var analysis = req.body.analysis
@@ -158,28 +171,33 @@ router.post('/update/results', function(req, res, next) {
 
 });
 
-router.post('/update/', function(req, res, next) {
+router.post('/update/', function (req, res, next)
+{
     const sampleID = req.body['_id'];
     delete req.body['_id']
-        // console.log(req.body)
+    // console.log(req.body)
     sample.updateElementByID(sampleID, req.body)
 });
 
-router.post('/create/', function(req, res, next) {
+router.post('/create/', function (req, res, next)
+{
 
     // console.log(req.body)
     sample.createElement(req.body)
         .then(
-            function(response) {
+            function (response)
+            {
                 res.json(response);
             }
         )
 });
 
-router.post('/remove/', function(req, res, next) {
+router.post('/remove/', function (req, res, next)
+{
     sample.removeElementByID(req.body['_id'])
         .then(
-            function(response) {
+            function (response)
+            {
                 res.json({
                     removed: true
                 })
@@ -187,7 +205,8 @@ router.post('/remove/', function(req, res, next) {
         );
 });
 
-router.get('/status/:sample_id/:project_id/:status', function(req, res) {
+router.get('/status/:sample_id/:project_id/:status', function (req, res)
+{
 
     if (req.params.status != "done") {
         // update the status
@@ -200,19 +219,22 @@ router.get('/status/:sample_id/:project_id/:status', function(req, res) {
         // when the process is done retrieve the results
         var cmd1 = "scp " + remote_host + req.params.project_id + "/" + req.params.sample_id + "/all.bestHit.json /src/data/" + req.params.project_id + "_" + req.params.sample_id + ".json";
         var cmd2 = "scp " + remote_host + req.params.project_id + "/" + req.params.sample_id + "/all.bestHit.min.json /src/data/" + req.params.project_id + "_" + req.params.sample_id + ".min.json";
+        var cmd3 = "scp " + remote_host + req.params.project_id + "/" + req.params.sample_id + "/all.bestHit-full.json /src/data/" + req.params.project_id + "_" + req.params.sample_id + "-full.json";
 
-        var cmd = cmd1 + ' && ' + cmd2;
+        var cmd = cmd1 + ' && ' + cmd2 + ' ' + cmd3;
 
         console.log(cmd);
 
-        var dir = exec(cmd, function(err, stdout, stderr) {
+        var dir = exec(cmd, function (err, stdout, stderr)
+        {
             if (err) {
                 console.log('error retrieving file');
             }
             console.log(stdout);
         });
 
-        dir.on('exit', function(code) {
+        dir.on('exit', function (code)
+        {
             // exit code is code
             console.log('updating sample' + req.params.sample_id, req.params.status);
             sample.updateElementByID(req.params.sample_id, {
